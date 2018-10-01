c2f22db84cb4f63b63de9ef4ae7c3640cd7e2018
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-01 16:41:58 -0300

    Validar campos de usuário e senha

---
+++ js/main.js
@@ -0,0 +1,15 @@
+let buttonSubmit = document.querySelector(".submit");
+buttonSubmit.addEventListener("click", function(event) {
+    event.preventDefault();
+    let user = document.querySelector("#user");
+    let password = document.querySelector("#password");
+    if(user.value === "") {
+        alert("Você não escreveu um usuário");
+    };
+    if(password.length < 6 || password.length != "") {
+        alert("Sua senha está muito curta");
+    };
+    if(password.value == "") {
+        alert("Você não escreveu uma senha")
+    }
+});
