01f713ac414fed819f0ed5e73744a7cc42b3d4f5
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-01 17:28:56 -0300

    Validar valor no campo

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ buttonSubmit.addEventListener("click", function(event) {
     event.preventDefault();
     let user = document.querySelector("#user");
     let password = document.querySelector("#password");
+    let value = document.querySelector("#value");
     if(user.value === "") {
         alert("Você não escreveu um usuário");
     };
     if(password.length < 6 || password.length != "") {
         alert("Sua senha está muito curta");
     };
-    if(password.value == "") {
-        alert("Você não escreveu uma senha")
-    }
+    if(value.value === "") {
+        alert("Selecione um valor");
+    };
 });
