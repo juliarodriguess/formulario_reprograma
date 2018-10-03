59f52905cfcd0005907c2981247ff66c4fc3a55e
Julia Rodrigues <srodriguesjulia@gmail.com>
2018-10-03 15:59:16 -0300

    Mensagens de erro e sucesso

--- js/main.js
+++ js/main.js
@@ -5,12 +5,27 @@ buttonSubmit.addEventListener("click", function(event) {
     let password = document.querySelector("#password");
     let value = document.querySelector("#value");
     if(user.value === "") {
-        alert("Você não escreveu um usuário");
+        //alert("Você não escreveu um usuário");
+        const userError = document.createElement("span");
+        const userErrorText = document.createTextNode("Você não escreveu um usuário");
+        userError.appendChild(userErrorText);
+        user.parentNode.insertBefore(userError, user);
     };
     if(password.length < 6 || password.length != "") {
-        alert("Sua senha está muito curta");
+        //alert("Sua senha está muito curta");
+        const passwordError = document.createElement("span");
+        const passwordErrorText = document.createTextNode("Sua senha está muito curta");
+        passwordError.appendChild(passwordErrorText);
+        password.parentNode.insertBefore(passwordError, password);
     };
     if(value.value === "") {
-        alert("Selecione um valor");
-    };
+        //alert("Selecione um valor");
+        const valueError = document.createElement("span");
+        const valueErrorText = document.createTextNode("Selecione um valor");
+        valueError.appendChild(valueErrorText);
+        value.parentNode.insertBefore(valueError, value);
+    } else {
+        alert("Cadastro efetuado com sucesso!")
+        buttonSubmit.disabled = true;
+    }
 });
